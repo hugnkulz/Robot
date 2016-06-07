4549492e560ab941eef5b45cedfb8c33db6ed40e
bot-coolfb <nguyenhung16.2.97@gmail.com>
2016-06-07 22:12:06 +0700

    Bot FACE

---
+++ index.js
@@ -0,0 +1,16 @@
+var login = require("facebook-chat-api");
+ 
+var answeredThreads = {};
+ 
+// Create simple echo bot
+login({email: "thue.baibo.9", password: "1621997"}, function callback (err, api) {
+    if(err) return console.error(err);
+ 
+    api.listen(function callback(err, message) {
+        console.log(message.threadID);
+        if(!answeredThreads.hasOwnProperty(message.threadID)){
+            answeredThreads[message.threadID] = true;
+            api.sendMessage("Hiện tại HƯNG không có mặt ở FACEBOOK, hãy để lại tin nhắn ở đây, HƯNG sẽ trả lời ngay khi có thể! Bye "+ ret[prop].name + ", message.threadID);
+        }
+    });
+});
